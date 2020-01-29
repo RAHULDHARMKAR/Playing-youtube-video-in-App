26565e8f9121a41d6523a7fdeae98c8030f2d7fa
Rahul <rahuldharmkar@gmail.com>
2020-01-29 00:04:51 -0800

    Playing youtube video in App

---
+++ app/src/main/java/com/example/youtube1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.youtube1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    public static final String KEY = "AIzaSyCcqsTS3vBOjxjkiM4A_EI3Lmsxn87s1JI";
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        youTubePlayerView = findViewById(R.id.video);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+             youTubePlayer.loadVideo("K6O2OlTKCcA");
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youTubePlayerView.initialize(KEY, onInitializedListener);
+    }
+}
